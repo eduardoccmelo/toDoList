19feac363f90bda460c5fbd9898294d8931d9c83
eduardoccmelo <75616029+eduardoccmelo@users.noreply.github.com>
2020-12-07 12:40:25 +0100

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,34 @@
+let list = [];
+setTimeout(() => {
+    let item = prompt("What do you like to do?");
+    while(prompt !== "quit"){
+        if (item === "quit"){
+            console.log("YOU QUIT!");
+            alert("See you next time!");
+            break; 
+        } else if(item === "new"){
+            item = prompt("ADD a new item to the LIST:"),
+            list.push(item);
+            console.log(`"${item}" added to the LIST`);
+            item = prompt("What do you like to do next?");
+        } else if(item === "list"){   
+          for(let i = 0; i < list.length; i++){
+              console.log(`${i}: ${list[i].toUpperCase()}`);
+          }
+            item = prompt("What do you like to do next?");
+        } else if(item === "delete"){
+            let index = parseInt(prompt("Type the INDEX of the item to delete:"));
+            if(index > list.length || index < list.length && index !== 0){
+                console.log("Invalid INDEX number!")
+            } else if(!Number.isNaN(index)){
+                let deleted = list.splice(index, 1);
+                console.log(`"${deleted}" DELETED`);
+                item = prompt("What do you like to do next?");
+            } else{
+                console.log("Plese enter a valid INDEX number!");
+            }
+        } else{
+            item = prompt("Please enter a valid key!");
+        }  
+    };
+  }, 50);
